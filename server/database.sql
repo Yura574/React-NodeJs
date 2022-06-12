dd21e84779175cb9ff14fe869639976e634479fe
Yura <yura5742248@gmail.com>
2022-06-12 16:20:18 +0300

    crud for posts

--- server/database.sql
+++ server/database.sql
@@ -1,14 +1,16 @@
-create TABLE person(
+create TABLE person
+(
     id      SERIAL PRIMARY KEY,
     name    VARCHAR(255),
     surname VARCHAR(255)
 );
 
-create TABLE posts(
-    id SERIAL PRIMARY KEY ,
-    title VARCHAR(255),
+create TABLE posts
+(
+    id      SERIAL PRIMARY KEY,
+    title   VARCHAR(255),
     content VARCHAR(255),
     user_id INTEGER,
-    FOREIGN KEY (user_id) REFERENCES person(id)
+    FOREIGN KEY (user_id) REFERENCES person (id)
 );
 
